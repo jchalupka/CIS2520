50c50f76b744fbafdbf03d597e4ca21adec0a4e1
Jordan Chalupka <jchalupk@mail.uoguelph.ca>
2016-05-27 03:11:44 -0400

    mes

--- Lab2ChalupkaJordan/include/test.h
+++ Lab2ChalupkaJordan/include/test.h
@@ -8,7 +8,7 @@
 #include <stdio.h>
 #include "ListADT.h"
 
-int testAddFront(int num, node * list);
+void testAddFront(int num, node * list);
 /*
 Pre: Test will reveal the correctness of the addFront function
 Post: None
@@ -18,27 +18,27 @@ node * testCreate(void);
 Pre: Test will reveal the correctness of the createList function
 Post: None
 */
-int testGetFrontValue(node * list);
+void testGetFrontValue(node * list);
 /*
 Pre: Test will reveal the correctness of the getFrontValue function
 Post: None
 */
-int testGetLength(int length, node * list);
+void testGetLength(int length, node * list);
 /*
 Pre: Test will reveal the correctness of the getLength function
 Post: None
 */
-int testPrintList(char * strList, node * list);
+void testPrintList(char * strList, node * list);
 /*
 Pre: Test will reveal the correctness of the printList function
 Post: None
 */
-int testRemoveFront(node * list);
+void testRemoveFront(node * list);
 /*
 Pre: Test will reveal the correctness of the removeFront function
 Post: None
 */
-int testAddFour(node * list);
+void testAddFour(node * list);
 /*
 Pre: Test will reveal the correctness of the addFour function
 Post: None
