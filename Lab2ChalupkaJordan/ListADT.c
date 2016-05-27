29fd910f2db9d00d6495bf45bc686326bb1e4b2c
Jordan Chalupka <jchalupk@mail.uoguelph.ca>
2016-05-27 01:53:06 -0400

    Lots of work done

---
+++ Lab2ChalupkaJordan/ListADT.c
@@ -0,0 +1,121 @@
+/*
+	Jordan Chalupka
+	0928258
+*/
+#include "listADT.h"
+
+/*Return a list ADT*/
+node * createList (void) {
+	node * list;
+	list = malloc(sizeof(node));
+	if (list == NULL) {
+		printf("An error has occured, not enough memory available.\n");
+		return NULL;
+	}
+	list->next = NULL;
+
+	return list;
+}
+
+/*Destory the list, freeing all of the associated memory*/
+void destroyList (node * theList) {
+	if (!isInit(theList)) {
+		return;
+	}
+
+	while (theList != NULL) {
+		//printf("%d\n",(theList->next)->nodeValue);
+		node * temp;
+		temp = theList;
+		theList = theList->next;
+
+		free(temp);		
+	}
+
+	return;
+}
+
+/*Initialize a node, mallocing all of the needed memory*/
+node * initNode (int value) {
+	node * newNode = malloc(sizeof(node));
+	newNode->nodeValue = value;
+	return newNode;
+}
+
+void addFront (node * theList, int value) {
+	if (!isInit(theList)) {
+		return;
+	}
+	node * newNode = initNode(value);
+	newNode->next = theList->next;
+	theList->next = newNode;
+
+	return;
+}
+
+/*Get the value of the front of the list*/
+int getFrontValue (node * theList) {
+	if (!isInit(theList)) {
+		srand(time(NULL));
+		return rand();
+	}
+	node * firstNode;
+	firstNode = theList->next;
+	int frontValue;
+	frontValue = firstNode->nodeValue;
+
+	return frontValue;
+}
+
+/*Get the length of the list*/
+int getLength (node * theList) {
+	if (!isInit(theList)) {
+		return 0;
+	}	
+	int length = 0;
+	while (theList->next != NULL) {
+		length++;
+		theList = theList->next;
+	}
+
+	return length;
+}
+
+/*Print out each of the nodes in the list*/
+void printList (node * theList) {
+	if (!isInit(theList)) {
+		return;
+	}
+	printf("LIST: ");
+	theList = theList->next;
+	while (theList != NULL) {
+		printf("[%d]",theList->nodeValue);
+		theList = theList->next;
+	}
+	printf("\n");
+
+	return;
+}
+
+/*Remove the first item in the list*/
+void removeFront (node * theList) {
+	if (!isInit(theList)) {
+		return;
+	}
+	if (theList->next == NULL) {
+		return;
+	}
+	node * firstNode = theList->next;
+	theList->next = firstNode->next; 
+
+	return;
+}
+
+/*Check if the node is initialized*/
+int isInit(node * theList) {
+	if (theList == NULL) {
+		printf("Error: List uninitialized\n");
+		return 0;
+	}
+	return 1;
+}
---
+++ Lab2ChalupkaJordan/test.h
@@ -0,0 +1,47 @@
+/*
+	Jordan Chalupka
+	0928258
+*/
+#ifndef __TEST__
+#define __TEST__
+
+#include <stdio.h>
+#include "listADT.h"
+
+void testAddFront(int num, node * list);
+/*
+Pre: Test will reveal the correctness of the addFront function
+Post: None
+*/
+node * testCreate(void);
+/*
+Pre: Test will reveal the correctness of the createList function
+Post: None
+*/
+void testGetFrontValue(node * list);
+/*
+Pre: Test will reveal the correctness of the getFrontValue function
+Post: None
+*/
+void testGetLength(int length, node * list);
+/*
+Pre: Test will reveal the correctness of the getLength function
+Post: None
+*/
+void testPrintList(char * strList, node * list);
+/*
+Pre: Test will reveal the correctness of the printList function
+Post: None
+*/
+void testRemoveFront(node * list);
+/*
+Pre: Test will reveal the correctness of the removeFront function
+Post: None
+*/
+void testAddFour(node * list);
+/*
+Pre: Test will reveal the correctness of the addFour function
+Post: None
+*/
+
+#endif
