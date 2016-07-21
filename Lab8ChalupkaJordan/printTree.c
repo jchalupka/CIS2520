b6f54c9e0fdbc354af07342977c178c7ce932db9
Jordan Chalupka <jchalupk@mail.uoguelph.ca>
2016-07-21 14:22:57 -0400

    mes

--- Lab8ChalupkaJordan/printTree.c
+++ Lab8ChalupkaJordan/printTree.c
@@ -20,7 +20,7 @@ void printTrees (int level, int curWidth, int initWidth, int leftOrRight) {
 		mvprintw(level, findMiddleX(curWidth, "X"),"X");
 	//} else {
 		// right
-		mvprintw(level, findMiddleX(curWidth, "X") + initWidth, "X");
+		mvprintw(level, findMiddleX(curWidth, "X") + curWidth, "X");
 	//}
 
 	// Print the right tree
