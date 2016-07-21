e6abadc55936902edc78c0a6cc920b0309e41857
Jordan Chalupka <jchalupk@mail.uoguelph.ca>
2016-07-21 10:31:34 -0400

    mes

--- Lab8ChalupkaJordan/printTree.c
+++ Lab8ChalupkaJordan/printTree.c
@@ -8,7 +8,7 @@ int findMiddleX(int maxX, char * word) {
 
 void printTrees (int level, int curWidth, int initWidth, int leftOrRight) {
 	// Recursive function to print trees
-	if (level == 4) {
+	if (level == 5) {
 		return;
 	}
 
