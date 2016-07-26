1549b1800931114e0a1b814c91f2c620d8332111
Jordan Chalupka <jchalupk@mail.uoguelph.ca>
2016-07-26 15:44:48 -0400

    mes

--- Lab8ChalupkaJordan/restaurant.c
+++ Lab8ChalupkaJordan/restaurant.c
@@ -94,8 +94,8 @@ void printData (void * data, int shift) {
 	}
 
 	//printf("%s\n%s\n%d\n\n", restPtr->name, restPtr->type, restPtr->rating);
-	for (int i = 0; i < shift-8; i++) printw(" ");
-	for (int j = 0; j < 4; j++) printw("-");
+	for (int i = 0; i < shift; i++) printw(" ");
+	//for (int j = 0; j < 4; j++) printw("-");
 	printw("%s %s %d", restPtr->name, restPtr->type, restPtr->rating);
 	refresh();
 
