7a6251b5a67fadedcc3a7edc30fd6c21ae675113
Jordan Chalupka <jchalupk@mail.uoguelph.ca>
2016-07-27 14:46:00 -0400

    mes

--- Lab8ChalupkaJordan/restaurant.c
+++ Lab8ChalupkaJordan/restaurant.c
@@ -161,7 +161,7 @@ void traverseInOrder (Tree * tree, int shift, int layer) {
 	int cury,curx;
 	getyx(stdscr,cury,curx);
 	vline('$', numLinesDown(tree) + 3);
-	move(cury,curx);
+	
 	
 
 	printw("\n");
