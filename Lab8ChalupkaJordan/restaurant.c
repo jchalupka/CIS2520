a159b993a337814dc4d554ee42ea31cafd106258
Jordan Chalupka <jchalupk@mail.uoguelph.ca>
2016-07-27 11:44:33 -0400

    mes

--- Lab8ChalupkaJordan/restaurant.c
+++ Lab8ChalupkaJordan/restaurant.c
@@ -110,16 +110,17 @@ void traverseInOrder (Tree * tree, int shift, int layer) {
 
 	printw("\n");
 
-	printData(getRootData(tree), shift);
-	printw(" %d\n", layer);
-	getchar();
-	
+
+
 	if (getRightSubtree(tree)) {
 		
 		traverseInOrder(getRightSubtree(tree), shift + 8, layer + 1);
 		
 	}
-
+	
+	printData(getRootData(tree), shift);
+	printw(" %d\n", layer);
+	getchar();
 	
 
 	if (getLeftSubtree(tree)) {
