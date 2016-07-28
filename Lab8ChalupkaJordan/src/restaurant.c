300e518b0232d28d252af4af1553287556f129d9
Jordan Chalupka <jchalupk@mail.uoguelph.ca>
2016-07-28 17:23:06 -0400

    mes

--- Lab8ChalupkaJordan/src/restaurant.c
+++ Lab8ChalupkaJordan/src/restaurant.c
@@ -128,7 +128,7 @@ void printData(void * tree, int shift) {
 	getyx(stdscr, cury, curx);
 	char word[255];
 	sprintf(word, "%s (%d)",restPtr->name, restPtr->rating);
-	move(cury,curx + shift + 10);
+	move(cury,curx + shift);
 	
 	printw("%s", word);
 
