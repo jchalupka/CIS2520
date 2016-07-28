ae9bae9949980767ace65194ea0a887f6ba86c03
Jordan Chalupka <jchalupk@mail.uoguelph.ca>
2016-07-28 14:51:14 -0400

    mes

--- Lab8ChalupkaJordan/src/restaurant.c
+++ Lab8ChalupkaJordan/src/restaurant.c
@@ -3,12 +3,12 @@
 #include "restaurant.h"
 
 Restaurant * createRestaurant (char * name, char * type, int rating) {
-	if (name == NULL|| type == NULL) {
+	if (name == NULL || type == NULL) {
 		printf("Incorrect name and or type pointer\n");
 
 		return NULL;
 	}
-	
+
 	Restaurant * restPtr = malloc(sizeof(Restaurant));
 	restPtr->name = malloc(sizeof(char)*255);
 	restPtr->type = malloc(sizeof(char)*255);
